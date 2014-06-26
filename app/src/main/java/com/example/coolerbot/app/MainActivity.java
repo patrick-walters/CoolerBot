1ece96101b405c48e8be1fb29f9bf9dfc3c51cfc
pwalters <waltersufl8@gmail.com>
2014-06-26 17:45:56 -0400

    Added velocity to state estimator.

--- app/src/main/java/com/example/coolerbot/app/MainActivity.java
+++ app/src/main/java/com/example/coolerbot/app/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.coolerbot.app;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -14,17 +20,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
     Button backwardButton;
     Button stopButton;
     SabertoothDriver sabertoothDriver;
+    StateEstimator stateEstimator;
     Context globalCotext;
+    Handler handler;
+
+    private Timer displayUpdate = new Timer();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.activity_main);
         globalCotext = this;
+        handler = new Handler();
 
         sabertoothDriver = new SabertoothDriver(this);
+        stateEstimator = new StateEstimator(this);
 
         forwardButton = (Button) findViewById(R.id.forwardButton);
         forwardButton.setOnClickListener(this);
@@ -37,21 +50,25 @@ public class MainActivity extends Activity implements View.OnClickListener {
         stopButton = (Button) findViewById(R.id.stopButton);
         stopButton.setOnClickListener(this);
 
+        displayUpdate.scheduleAtFixedRate(new UpdateDisplayTask(), 1000, 50);
     }
 
     @Override
     protected void onResume() {
         sabertoothDriver.resumeDriver();
+        stateEstimator.registerListeners();
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        super.onResume();
+        stateEstimator.unregisterListeners();
+        super.onPause();
     }
 
     @Override
     protected void  onStop() {
+        stateEstimator.unregisterListeners();
         super.onStop();
     }
 
@@ -77,8 +94,31 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 sabertoothDriver.setBackwardMixed((byte)50);
                 break;
             case R.id.stopButton:
-                sabertoothDriver.setForwardMixed((byte)00);
+                sabertoothDriver.setForwardMixed((byte)0);
                 break;
+        }
+    }
+
+    private Runnable updateDisplay = new Runnable() {
+        public void run() {
+            float[] orientation = stateEstimator.getOrientation();
+            TextView fusedAzimuth = (TextView) findViewById(R.id.azimuth);
+            fusedAzimuth.setText("Azimuth: " + (float) (orientation[0] * 180.0 / 3.143));
+
+            float fusedSpeed = stateEstimator.getSpeed();
+            TextView fusedSpeedView = (TextView) findViewById(R.id.speed);
+            fusedSpeedView.setText("Fused Speed: " + fusedSpeed);
+
+            float gpsSpeed = stateEstimator.getGpsSpeed();
+            TextView gpsSpeedView = (TextView) findViewById(R.id.gpsSpeed);
+            gpsSpeedView.setText("GPS Speed: " + gpsSpeed);
+        }
+    };
+
+    private class UpdateDisplayTask extends TimerTask {
+        @Override
+        public void run() {
+            handler.post(updateDisplay);
         }
     }
 }
